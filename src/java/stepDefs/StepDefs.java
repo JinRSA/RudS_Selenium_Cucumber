433b2ca361ebdb659c639635215640c730588e61
Jin <Jin_.96@mail.ru>
2021-07-12 18:06:58 +0400

    Code refactoring.

--- src/java/stepDefs/StepDefs.java
+++ src/java/stepDefs/StepDefs.java
@@ -11,23 +11,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class StepDefs {
-    private WebDriver driver;
+    private WebDriver m_driver;
+    private String m_object, m_city;
 
     @Пусть("открыт ресурс авито")
     public void открытРесурсАвито() {
-        String path2ChromeDriver = "C:\\My\\Projects\\Java\\Libraries\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", path2ChromeDriver);
+        try (InputStream input = new FileInputStream("src/test/resources/resources.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+            String path2ChromeDriver = properties.getProperty("path2ChromeDriver");
+            System.setProperty("webdriver.chrome.driver", path2ChromeDriver);
+        } catch (Exception ignored) {}
 
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-//        driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
-        driver.get("https://avito.ru");
-        driver.manage().window().maximize();
+        m_driver = new ChromeDriver();
+        m_driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        m_driver.get("https://avito.ru");
+        m_driver.manage().window().maximize();
     }
 
     @ParameterType(".*")
@@ -40,46 +47,59 @@ public class StepDefs {
 
     @И("в выпадающем списке категорий выбрана {category}")
     public void вВыпадающемСпискеКатегорийВыбранаОргтехникаИРасходники(Category categ) {
-        Select categoryDropDownList = new Select(driver.findElement(By.id("category")));
+        By by = By.id("category");
+        Select categoryDropDownList = new Select(m_driver.findElement(by));
         categoryDropDownList.selectByVisibleText(categ.getValue());
     }
 
     @И("в поле поиска введено значение {string}")
     public void вПолеПоискаВведеноЗначение(String value) {
-        WebElement search = driver.findElement(By.xpath("//input[@data-marker=\"search-form/suggest\"]"));
+        m_object = value;
+        By by = By.xpath("//input[@data-marker=\"search-form/suggest\"]");
+        WebElement search = m_driver.findElement(by);
         search.sendKeys(value);
     }
 
     @Тогда("кликнуть по выпадающему списку региона")
     public void кликнутьПоВыпадающемуСпискуРегиона() {
-        driver.findElement(By.xpath("//div[@data-marker=\"search-form/region\"]")).click();
+        By by = By.xpath("//div[@data-marker=\"search-form/region\"]");
+        m_driver.findElement(by).click();
     }
 
     @Тогда("в поле регион введено значение {string}")
     public void вПолеРегионВведеноЗначение(String cityStr) {
-        WebElement searchCity = driver.findElement(By.xpath("//input[@data-marker=\"popup-location/region/input\"]"));
+//        m_city = cityStr;
+        By by = By.xpath("//input[@data-marker=\"popup-location/region/input\"]");
+        WebElement searchCity = m_driver.findElement(by);
         searchCity.sendKeys(cityStr);
-        WebElement city = driver.findElement(By.xpath("//strong[text()=\"" + cityStr + "\"]"));
+        by = By.xpath("//strong[text()=\"" + cityStr + "\"]");
+        WebElement city = m_driver.findElement(by);
+        by = By.xpath("//span[strong[text()=\""+ cityStr + "\"]]");
+        m_city = city.findElement(by).getText();
         city.click();
     }
 
     @И("нажата кнопка показать объявления")
     public void нажатаКнопкаПоказатьОбъявления() {
-        driver.findElement(By.xpath("//button[@data-marker=\"popup-location/save-button\"]")).click();
+        By by = By.xpath("//button[@data-marker=\"popup-location/save-button\"]");
+        m_driver.findElement(by).click();
     }
 
     @Тогда("открылась страница результаты по запросу {string}")
     public void открыласьСтраницаРезультатыПоЗапросу(String value) {
-        String result = driver.findElement(By.xpath("//h1[@data-marker=\"page-title/text\"]")).getText();
+        By by = By.xpath("//h1[@data-marker=\"page-title/text\"]");
+        String result = m_driver.findElement(by).getText();
         assert result.contains(value) : "Страница с результатами по запросу \"" + value + "\" не найдена.";
     }
 
     @И("активирован чекбокс только с фотографией")
     public void активированЧекбоксТолькоСФотографией() {
-        WebElement checkBox = driver.findElement(By.xpath("//input[@data-marker=\"delivery-filter/input\"]"));
+        By by = By.xpath("//input[@data-marker=\"delivery-filter/input\"]");
+        WebElement checkBox = m_driver.findElement(by);
         if (!checkBox.isSelected())
             checkBox.sendKeys(Keys.SPACE);
-        driver.findElement(By.xpath("//button[@data-marker=\"search-filters/submit-button\"]")).click();
+        by = By.xpath("//button[@data-marker=\"search-filters/submit-button\"]");
+        m_driver.findElement(by).click();
     }
 
     @ParameterType(".*")
@@ -92,18 +112,22 @@ public class StepDefs {
 
     @И("в выпадающем списке сортировка выбрано значение {sortBy}")
     public void вВыпадающемСпискеСортировкаВыбраноЗначениеДороже(SortBy sortBy) {
-        Select filterByDropDownList = new Select(driver.findElement(By.xpath("//select[option[./text()=\"По умолчанию\"]]")));
+        By by = By.xpath("//select[option[./text()=\"По умолчанию\"]]");
+        Select filterByDropDownList = new Select(m_driver.findElement(by));
         filterByDropDownList.selectByVisibleText(sortBy.getBy());
     }
 
     @И("в консоль выведено значение названия и цены {int} первых товаров")
     public void вКонсольВыведеноЗначениеНазванияИЦеныПервыхТоваров(int count) {
-        List<WebElement> resultsName = driver.findElements(By.xpath("//h3[@itemprop=\"name\"]"));
-        List<WebElement> resultsPrice = driver.findElements(By.xpath("//span[@data-marker=\"item-price\"]"));
+        By by = By.xpath("//h3[@itemprop=\"name\"]");
+        List<WebElement> resultsName = m_driver.findElements(by);
+        by = By.xpath("//span[@data-marker=\"item-price\"]");
+        List<WebElement> resultsPrice = m_driver.findElements(by);
         assert count <= resultsName.size() : count + ">" + resultsName.size() + "! Значение должно быть в пределах количества выводимых на странице результатов";
-        for (int i = 0; i < count; ++i) {
-            System.out.println("Наименование: " + resultsName.get(i).getText() + "\nЦена: " + resultsPrice.get(i).getText());
-        }
-        driver.quit();
+        String output = m_city + "; " + m_object + '.';
+        for (int i = 0; i < count; ++i)
+            output += "\nНаименование: " + resultsName.get(i).getText() + "\nЦена: " + resultsPrice.get(i).getText();
+        System.out.println(output);
+        m_driver.quit();
     }
 }
